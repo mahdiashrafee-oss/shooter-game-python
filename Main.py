05eb385922b1ca92c7eb9baafc4f4fca5474301c
mahdiashrafee <mahdiash@buffalo.edu>
2021-03-26 12:30:03 -0400

    feat: variable definitions added

--- Main.py
+++ Main.py
@@ -1,1 +1,27 @@
-print("done deal")
+import pygame
+
+import random
+
+screen = pygame.display.set_mode((480, 640))
+
+shoot = pygame.image.load('bloon.png')
+
+charecter = pygame.image.load('charecter.png')
+
+me = pygame.transform.scale(lala, (400, 240))
+
+shoot1 = pygame.transform.scale(shoot, (120, 180))
+
+bkground = pygame.image.load('background.jpg')
+
+bkground = pygame.transform.scale(bkground,(480,640))
+
+user = 70
+
+tuser = 500
+
+
+
+keep_alive = True
+
+
