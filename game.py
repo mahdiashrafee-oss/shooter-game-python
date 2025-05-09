d25e5727c9f729ead08437ca2fa6fbce70a39617
mahdiashrafee <mahdiash@buffalo.edu>
2025-05-09 18:30:41 -0400

    refactor: move assets to folder and update code references

--- game.py
+++ game.py
@@ -3,11 +3,11 @@ import random
 
 
 screen = pygame.display.set_mode((480, 640))
-shoot = pygame.image.load('bloon.png')
-charecter = pygame.image.load('charecter.png')
+shoot = pygame.image.load('./assests/bloon.png')
+charecter = pygame.image.load('./assests/charecter.png')
 me = pygame.transform.scale(lala, (400, 240))
 shoot1 = pygame.transform.scale(shoot, (120, 180))
-bkground = pygame.image.load('background.jpg')
+bkground = pygame.image.load('./assests/background.jpg')
 bkground = pygame.transform.scale(bkground,(480,640))
 user = 70
 tuser = 500
